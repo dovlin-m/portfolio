3d8122d814ebf1322913313668630112f9c758d7
m.davidov <m.davidov@eco-fin.eu>
2023-06-23 17:57:26 +0300

    feat: init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -183,6 +183,7 @@ export default {
   globalName: process.env.npm_package_name,
   server: { port: process.env.PORT, host: process.env.HOST },
   env: process.env,
+  publicPath: '/portfolio/',
   publicRuntimeConfig,
 
   head,
