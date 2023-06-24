fa56fb2b0cbc367be42a77f2833cdb8e7140f0dd
m.davidov <m.davidov@eco-fin.eu>
2023-06-24 16:53:16 +0300

    feat: init

--- components/molecules/card/MenuDropdown/MenuDropdown.vue
+++ components/molecules/card/MenuDropdown/MenuDropdown.vue
@@ -12,7 +12,7 @@ export default defineComponent({
   async fetch () {
     this.menu =
       await this.$content(`${this.$i18n.locale}/portfolio`, { deep: true }).only(['id', 'title']).fetch();
-    console.log('title', this.menu);
+    // console.log('title', this.menu);
   },
 
   beforeMount () {
@@ -25,8 +25,8 @@ export default defineComponent({
   },
 
   mounted () {
-    console.log('menu', this.menu);
-    console.log('locale', this.$i18n.locale);
+    // console.log('menu', this.menu);
+    // console.log('locale', this.$i18n.locale);
   },
 
   methods: {
